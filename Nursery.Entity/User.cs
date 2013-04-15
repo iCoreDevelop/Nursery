4fc4900a9846f31b97e30dc8432aca38bd9cbae7
Oscar Perez Lara <iCoreDevelop@gmail.com>
2013-04-15 11:09:00 -0500

    Agregar linea

--- Nursery.Entity/User.cs
+++ Nursery.Entity/User.cs
@@ -11,6 +11,8 @@ namespace Nursery.Entity
         public string UserName { get; set; }
         public string Password { get; set; }
         public string Status { get; set; }
+        public string Status2 { get; set; }
+
 
         public void Dispose()
         {
